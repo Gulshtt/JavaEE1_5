d756f24ae7b72fc836f95ee7cd7ae66b90373873
Gulshtt <gulshat.yesset@gmail.com>
2024-02-03 18:16:01 +0600

    Last

--- src/servlets/HomeServlet.java
+++ src/servlets/HomeServlet.java
@@ -1,16 +1,14 @@
 package servlets;
 
+import db.DBManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import models.Developer;
-
+import models.User;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @WebServlet("/home")
@@ -21,42 +19,36 @@ public class HomeServlet extends HttpServlet {
             response.setContentType("text/html");
             PrintWriter pw = response.getWriter();
 
-            pw.print("<form action=\"/home\" method=\"post\">");
+            pw.print("<form action='/home' method='post'>");
             pw.print("<label>NAME:</label>");
-            pw.print("<input name=\"name\" type=\"text\" style=\"margin-left:30px;\">");
+            pw.print("<input name='name' type='text' style='margin-left:30px;'>");
             pw.print("<br>");
             pw.print("<label>SURNAME:</label>");
-            pw.print("<input name=\"surname\" type=\"text\" style=\"margin-left:30px;\">");
+            pw.print("<input name='surname' type='text' style='margin-left:30px;'>");
+            pw.print("<br>");
+            pw.print("<label>FOOD:</label>");
+            pw.print("<select name='food'>");
+            pw.print("<option value='Manty-900 KZT'>Manty-900 KZT</option>");
+            pw.print("<option value='Pelmen-600 KZT'>Pelmen-600 KZT</option>");
+            pw.print("<option value='Lagman-800 KZT'>Lagman-800 KZT</option>");
+            pw.print("</select>");
             pw.print("<br>");
             pw.print("<button>SEND</button>");
             pw.print("</form>");
+            if(DBManager.user.getName()!=null){
+                User u = DBManager.user;
+                pw.print("<h>" + u.getName() +" "+ u.getSurname() +" is ordered " + u.getFood() + "</h>");
+            }
+    }
 
-       /* for(int i; i=0; i<10){
-            pw.println("<h1>Salem alem!</h1>");
-        }
-        */
-/*
-        List<Developer> developers = new ArrayList<>();
-        developers.add(new Developer("Даулет", "Д", "IT", 500000));
-        developers.add(new Developer("Тили", "Т", "IT", 500000));
-        developers.add(new Developer("Амир", "А", "IT", 500000));
-        developers.add(new Developer("Абылай", "А", "IT", 500000));
-        developers.add(new Developer("Дарын", "Д", "IT", 500000));
-        developers.add(new Developer("Жансая", "Ж", "IT", 500000));
-        developers.add(new Developer("Дина", "Д", "IT", 500000));
-        developers.add(new Developer("Гульшат", "Г", "IT", 500000));
-        developers.add(new Developer("Баглан", "Б", "IT", 500000));
-        developers.add(new Developer("Райхан", "Р", "IT", 500000));
-        developers.add(new Developer("Асыл", "А", "IT", 500000));
-        developers.add(new Developer("Альбина", "А", "IT", 500000));
-        developers.add(new Developer("Куляш", "К", "IT", 500000));
-        developers.add(new Developer("Ажара", "А", "IT", 500000));
-        developers.add(new Developer("Арман", "А", "IT", 500000));
-        developers.add(new Developer("Ерасыл", "Е", "IT", 500000));
-        developers.add(new Developer("Асель", "А", "IT", 500000));
-
-        request.setAttribute("razraby",developers);
-        //request.getRequestDispatcher("home.jsp").forward(request, response);
-*/
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+        String food = req.getParameter("food");
+        DBManager.user.setName(name);
+        DBManager.user.setSurname(surname);
+        DBManager.user.setFood(food);
+        resp.sendRedirect("/home");
     }
 }
